c78bcc080b3233917dcc8f0b9cb40d53480e309b
Maniakrzelaza <maniakrzelaza@gmail.com>
2018-04-30 12:59:42 +0200

    four

--- VSfirstdotnet/Program.cs
+++ VSfirstdotnet/Program.cs
@@ -22,6 +22,7 @@ namespace VSfirstdotnet
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                 .UseUrls("http://localhost:55726/")
                 .Build();
     }
 }
